1f2ceab489800f066c603ce277294a0d1f2f8bda
TIAS23 <96928599+TIAS23@users.noreply.github.com>
2023-10-01 12:25:01 +0700

    Initial commit

---
+++ app/src/main/java/com/tiashl/appsti_msib/DataItemType.kt
@@ -0,0 +1,10 @@
+package com.tiashl.appsti_msib
+
+class DataItemType {
+
+    companion object{
+        const val BEST_SELLER = 0
+        const val CLOTHING = 1
+        const val BANNER = 2
+    }
+}
